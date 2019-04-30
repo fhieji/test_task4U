4cb97daac7d8067830e312bcac407bed3418739c
TanakaMana <tnmn@TanakaMananoMacBook-Pro.local>
2019-04-30 11:54:13 +0900

    [fix] bug

--- app/controllers/project_technologies_controller.rb
+++ app/controllers/project_technologies_controller.rb
@@ -2,7 +2,7 @@ class ProjectTechnologiesController < ApplicationController
   private
 
   def task_params
-    params.require(:project).permit(:name, :description :tag_list) 
+    params.require(:project).permit(:name, :description, :tag_list) 
     #tag_list を追加
   end
 end
