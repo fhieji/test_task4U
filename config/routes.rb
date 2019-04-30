404ea13ed256d55ad929aba51aa26357bcfabb4d
TanakaMana <tnmn@TanakaMananoMacBook-Pro.local>
2019-04-30 16:01:19 +0900

    [modify] built-in front of tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
-  root to: 'projects#index'
+  root to: 'dashboards#index'
   resources :projects
   resources :tickets
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
